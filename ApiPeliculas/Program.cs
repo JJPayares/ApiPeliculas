5b278c2e5c5df9b8b0b7a32c80198f8b3cd7ab49
Juan Jose Payares <jpayares@lsv-tech.com>
2024-10-15 14:53:42 -0500

    progress

--- ApiPeliculas/Program.cs
+++ ApiPeliculas/Program.cs
@@ -1,7 +1,13 @@
+using ApiPeliculas.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container.((
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(   
+                        builder.Configuration.GetConnectionString(""))); 
+    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
